fd846baf88dc456d4ce4cfe0ba75d052f15b4ad1
Rainer Simon <rainer@rainersimon.io>
2024-03-20 17:06:51 +0100

    First pass at a 'document map' with all places

--- src/DocumentMapExtension/components/DocumentMap.tsx
+++ src/DocumentMapExtension/components/DocumentMap.tsx
@@ -1,4 +1,5 @@
-import type { SupabaseAnnotation } from '@recogito/annotorious-supabase';
+import { useMemo, useEffect } from 'react';
+import type { SupabaseAnnotation, SupabaseAnnotationBody } from '@recogito/annotorious-supabase';
 import { useAnnotations } from '@annotorious/react';
 import type { PluginInstallationConfig } from '@components/Plugins';
 import { useLeaflet } from '../../useLeaflet';
@@ -15,9 +16,38 @@ export const DocumentMap = (props: DocumentMapProps) => {
 
   const annotations = useAnnotations<SupabaseAnnotation>();
 
-  const { basemap} = props.plugin.meta.options;
+  const geotags = useMemo(() => annotations
+    .reduce<SupabaseAnnotationBody[]>((all, annotation) => {
+      const bodies = annotation.bodies.filter(b => b.purpose === 'geotagging' && b.value);
+      return [...all, ...bodies];
+    }, [])
+  , [annotations]);
+
+  const { basemap } = props.plugin.meta.options;
 
   const { ref, map } = useLeaflet({ basemap, initialCenter: [0, 0], initialZoom: 2 });
+
+  useEffect(() => {
+    if (!map || !geotags || geotags.length === 0) return;
+
+    const features = geotags
+      .map(b => JSON.parse(b.value!))
+      .filter(f => f.geometry?.coordinates);
+
+    const markers = features.map(feature => {
+      const [lon, lat] = feature.geometry.coordinates; 
+
+      // TODO popup
+      
+      return L.marker([lat, lon]);
+    });
+
+    const layer = L.layerGroup(markers).addTo(map);
+
+    return () => {
+      map.removeLayer(layer);
+    }
+  }, [geotags, map]);
 
   return (
     <div ref={ref} className="ou-gtp-document-map">
