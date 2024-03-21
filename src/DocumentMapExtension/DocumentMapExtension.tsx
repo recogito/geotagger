00ab9e758357500fd3b587a336050bb7ff5dbcbf
Rainer Simon <rainer@rainersimon.io>
2024-03-21 10:34:34 +0100

    Document map title bar + close button

--- src/DocumentMapExtension/DocumentMapExtension.tsx
+++ src/DocumentMapExtension/DocumentMapExtension.tsx
@@ -1,13 +1,13 @@
 import * as Dialog from '@radix-ui/react-dialog';
-import { Globe } from '@phosphor-icons/react';
+import { Globe, X } from '@phosphor-icons/react';
+import type { AnnotationToolbarExtensionProps } from '@components/Plugins';
 import { DocumentMap } from './components/DocumentMap';
-import type { ExtensionProps } from '@components/Plugins';
 
 import './DocumentMapExtension.css';
 
 import 'leaflet/dist/leaflet.css';
 
-export const DocumentMapExtension = (props: ExtensionProps) => {
+export const DocumentMapExtension = (props: AnnotationToolbarExtensionProps) => {
 
   return (
     <Dialog.Root>
@@ -21,7 +21,17 @@ export const DocumentMapExtension = (props: ExtensionProps) => {
         <Dialog.Content 
           className="dialog-content ou-gtp-document-map-container not-annotatable">
 
+          <Dialog.Title className="dialog-title">
+            {props.document.name}
+          </Dialog.Title>
+
           <DocumentMap plugin={props.plugin} />
+
+          <Dialog.Close className="dialog-close" asChild>
+            <button className="unstyled icon-only">
+              <X size={20} />
+            </button>
+          </Dialog.Close>
          
         </Dialog.Content>
       </Dialog.Portal>
