e0f16dcebd46532e0ec68421cf95c74555b6850f
Rainer Simon <rainer@rainersimon.io>
2024-09-17 16:19:27 +0200

    WHG robustness

--- src/useGazetteer/gazetteers/whGazetteer.ts
+++ src/useGazetteer/gazetteers/whGazetteer.ts
@@ -5,11 +5,18 @@ export const createWHGazetteer = (): Gazetteer => {
   const search = (query: string): Promise<GeoJSONFeature[]> =>
     fetch(`https://whgazetteer.org/api/index/?name=${query}`)
       .then(res => res.json())
-      .then(results => results.features.map((f: GeoJSONFeature) => ({
-        ...f,
-        // @ts-ignore
-        id: `https://whgazetteer.org/places/${f.properties['index_id']}/portal`
-      })));
+      .then(results => 
+        results.features.map((f: GeoJSONFeature) => {
+          return {
+            ...f,
+            // @ts-ignore
+            id: `https://whgazetteer.org/places/${f.properties['index_id']}/portal`
+          };
+        }).filter((f: GeoJSONFeature) => {
+          // Only support point geometries for now
+          return Boolean(f.geometry) && f.geometry.type === 'Point'
+        })
+      );
 
   return { search };
 
